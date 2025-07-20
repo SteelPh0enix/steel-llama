f2b75365092096893ff2152c5c278dd5fe024de4
SteelPh0enix <wojciech_olech@hotmail.com>
2025-07-20 21:44:44 +0200

    Implemented thinking output processing

--- bot/__init__.py
+++ bot/__init__.py
@@ -12,8 +12,48 @@ class Bot(commands.Bot):
         self.config = config
 
 
-def process_raw_response(response: str, model_config: ModelConfig) -> str:
-    return ""
+def process_thinking_output(
+    response: str, model_config: ModelConfig, replacement: str = "*"
+) -> str:
+    if (
+        model_config.thinking_prefix is not None
+        and model_config.thinking_suffix is not None
+    ):
+        thinking_prefix_position = response.find(model_config.thinking_prefix)
+
+        # thinking tags may be followed with whitespace, which must be removed
+        # otherwise, Discord won't render the text properly if replacement is used for formatting (like '*')
+        if thinking_prefix_position != -1:
+            # special case - if thinking prefix is the only part of the message.
+            if response.strip() == model_config.thinking_prefix:
+                return f"{replacement}Thinking...{replacement}"
+
+            response = (
+                replacement
+                + response[
+                    thinking_prefix_position + len(model_config.thinking_prefix) :
+                ].lstrip()
+            )
+
+            thinking_suffix_position = response.find(model_config.thinking_suffix)
+            if thinking_suffix_position != -1:
+                response = (
+                    response[:thinking_suffix_position].rstrip()
+                    + replacement
+                    + response[
+                        thinking_suffix_position + len(model_config.thinking_suffix) :
+                    ]
+                )
+            else:
+                response = response.rstrip() + replacement
+
+    return response
+
+
+def process_raw_response(response: str, model_config: ModelConfig | None) -> str:
+    if model_config is not None:
+        return process_thinking_output(response, model_config)
+    return response
 
 
 async def process_llm_response(
@@ -22,24 +62,16 @@ async def process_llm_response(
     bot_config: BotConfig,
     model_config: ModelConfig | None,
 ):
-    raw_response = ""
     response = ""
     last_edit_time = time.time()
 
     for chunk in response_stream:
-        raw_response += chunk["response"]
+        response += chunk["response"]
 
-        response = (
-            process_raw_response(raw_response, model_config)
-            if model_config is not None
-            else raw_response
-        )
-
-        now = time.time()
-        if now - last_edit_time >= bot_config.edit_delay:
-            await message.edit(content=response)
-            last_edit_time = now
+        if time.time() - last_edit_time >= bot_config.edit_delay:
+            await message.edit(content=process_raw_response(response, model_config))
+            last_edit_time = time.time()
 
     # process last chunk
     if response != "":
-        await message.edit(content=response)
+        await message.edit(content=process_raw_response(response, model_config))
